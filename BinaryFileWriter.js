4ff6071f5338bd573fe570e9c7951822443708ad
pwlin <pwlin05@gmail.com>
2013-11-12 13:55:53 +0100

    fixed js file

--- BinaryFileWriter.js
+++ BinaryFileWriter.js
@@ -1,24 +1,12 @@
-(function() {
+var exec = require("cordova/exec");
 
-	var cordovaRef = window.PhoneGap || window.Cordova || window.cordova; // old to new fallbacks
+function BinaryFileWriter() {};
 
-	function BinaryFileWriter() {
-		this.writeToFile = function(path, data, callback) { // data = base64 encoded string.
-			cordova.exec(callback, function(err) {
-				console.log('error.');
-			}, "BinaryFileWriter", "writeToFile", [path, data]);
-		};
-	}
+BinaryFileWriter.prototype.writeToFile = function(path, data, callback) { // data = base64 encoded string.
+	cordova.exec(callback, function(err) {
+		console.log('error.');
+	}, "BinaryFileWriter", "writeToFile", [path, data]);
+};
 
-	if (cordovaRef && cordovaRef.addConstructor) {
-		cordovaRef.addConstructor(function() {
-			if (!window.plugins) {
-				window.plugins = {};
-			}
-			if (!window.plugins.binaryFileWriter) {
-				window.plugins.binaryFileWriter = new BinaryFileWriter();
-			}
-		});
-	}
-
-})();
+var binaryFileWriter = new BinaryFileWriter();
+module.exports = binaryFileWriter;
